aec904440eb72adc40c40a187cfefea1d23ee1d8
rafael pineda <=>
2020-11-23 21:15:58 -0500

    repaso 23-11-2020

---
+++ main.js
@@ -0,0 +1,64 @@
+let Nombre="Rafael Enrique";
+let Apellido="Pineda Santiago";
+let Edad=22;
+let Genero="Masculino";
+let Estado_civil="Soltero";
+let Estatura=1.69;
+let Peso=68;
+let Color_de_pelo="Negro";
+let Color_de_ojos="Cafe";
+let Color_de_piel="morena"
+
+
+
+console.log("")
+console.log("Mi nombre es: "+Nombre)
+console.log("mi apellido es: "+Apellido);
+console.log("mi edad es: "+Edad);
+console.log("Mi genero es: "+Genero);
+console.log("Mi estado_civil es: "+Estado_civil);
+console.log("Mi estatura es "+Estatura);
+console.log("Mi peso es: "+Peso);
+console.log("Mi color de pelo es "+Color_de_pelo);
+console.log("Mi color de ojos es "+Color_de_ojos);
+console.log("Micolor de peiel es "+Color_de_piel);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let vaso, nombre="Rafael"; apellido, edad;
+
+
+vaso="agua";
+
+apellido ="pineda"
+vaso="jugo";
+edad="29"
+
+
+
+console.log(vaso);
+
+
+
+
+vaso = "coca cola";
+console.log(vaso);
+
+
+let nombre_sting="Rafael";
+let edad_number=22;
+
+console.log(typeof(nombre_sting), nombre_sting);
+console.log(typeof(edad_number), edad_number);
+console.log(typeof(genero_masculino_bool), genero_mascuino_boolean);
+*/
